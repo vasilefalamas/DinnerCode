1f93425ba880aaaad3fd55bcd3623bda612c1321
vasilefalamas <vasile.falamas@iquestgroup.com>
2014-10-01 22:32:56 +0300

    Added Manager.

---
+++ HolidayApproval/HolidayApproval/Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Mail;
+using HolidayApproval.HolidayRequest;
+using HolidayApproval.HolidayResponse;
+
+namespace HolidayApproval
+{
+    public class Manager
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public void Approve(Request request)
+        {
+            var response = CreateResponse(request, Status.Approved);
+            Respond(response);
+        }
+
+        public void Reject(Request request)
+        {
+            var response = CreateResponse(request, Status.Rejected);
+            Respond(response);
+        }
+
+        private Response CreateResponse(Request request, Status status)
+        {
+            return new Response
+            {
+                InitialRequest = request,
+                Status = status,
+                SignedBy = this.Name
+            };
+        }
+
+        private static void Respond(Response response)
+        {
+            if (response.Status == Status.Approved)
+            {
+                SendEmail();
+            }
+            else
+            {
+                SendEmail();
+            }
+        }
+
+        private static void SendEmail()
+        {
+
+        }
+    }
+}
