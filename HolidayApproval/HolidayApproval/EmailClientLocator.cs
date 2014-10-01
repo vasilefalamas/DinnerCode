25b33b7fe333424d5fae8e1d9db7c155588b7f7e
vasilefalamas <vasile.falamas@iquestgroup.com>
2014-10-01 23:53:14 +0300

    Added EmailClientLocator

---
+++ HolidayApproval/HolidayApproval/EmailClientLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HolidayApproval.Mail;
+
+namespace HolidayApproval
+{
+    public class EmailClientLocator
+    {
+        private static IEmailClient _emailClient;
+
+        
+        public static IEmailClient EmailClient
+        {
+            get
+            {
+                return _emailClient;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Email client argument was null.");
+                }
+                
+                _emailClient = value;
+            }
+        }
+    }
+}
