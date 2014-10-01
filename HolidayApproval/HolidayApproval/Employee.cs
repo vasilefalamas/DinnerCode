166ec259576f61b55ec02fb3de13feb31c739f2b
vasilefalamas <vasile.falamas@iquestgroup.com>
2014-10-01 22:44:42 +0300

    Added Holiday Apply action for Employee.

--- HolidayApproval/HolidayApproval/Employee.cs
+++ HolidayApproval/HolidayApproval/Employee.cs
@@ -1,9 +1,38 @@
-﻿namespace HolidayApproval
+﻿using System;
+using HolidayApproval.HolidayRequest;
+
+namespace HolidayApproval
 {
     public class Employee
     {
         public string Name { get; set; }
 
         public string Email { get; set; }
+
+        public void Apply(string managerEmail, DateTime start, DateTime end)
+        {
+            var request = CreateRequest(managerEmail, start, end);
+            
+            SendRequest(request);
+        }
+
+        private Request CreateRequest(string managerEmail, DateTime start, DateTime end)
+        {
+            return new Request
+            {
+                ManagerEmail = managerEmail,
+                Employee = this,
+                Bounds = new Bounds
+                {
+                    Start = start,
+                    End = end
+                }
+            };
+        }
+
+        private void SendRequest(Request request)
+        {
+
+        }
     }
 }
