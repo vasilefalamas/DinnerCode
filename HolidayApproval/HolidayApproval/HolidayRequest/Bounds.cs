ad94c0d28f168c7770f733cf4e7868bbf9088d57
vasilefalamas <vasile.falamas@iquestgroup.com>
2014-10-01 22:13:14 +0300

    Added Holiday Request.

---
+++ HolidayApproval/HolidayApproval/HolidayRequest/Bounds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolidayApproval.HolidayRequest
+{
+    public class Bounds
+    {
+        public DateTime Start {get; set; }
+
+        public DateTime End { get; set; }
+
+    }
+}
---
+++ HolidayApproval/HolidayApproval/HolidayRequest/Request.cs
@@ -0,0 +1,12 @@
+﻿namespace HolidayApproval.HolidayRequest
+{
+    public class Request
+    {
+        public Employee Employee { get; set; }
+
+        public string ManagerMail { get; set; }
+
+        public Bounds Bounds { get; set; }
+
+    }
+}
