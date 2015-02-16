ddd4842bb2f860ff28b769841ca35a4808c15824
Vasile Falamas <vasile.falamas@hotmail.com>
2015-02-16 23:41:58 +0200

    Added AccessToken.

---
+++ EventsNow.Connectivity/Authentication/AccessToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventsNow.Connectivity.Authentication
+{
+    public class AccessToken
+    {
+        public string Value { get; private set; }
+
+        public DateTime Expiry { get; private set; }
+
+        public AccessToken(string value, DateTime expiry)
+        {
+            Value = value;
+            Expiry = expiry;
+        }
+    }
+}
