34a1eaeffe7605eed1a5830b76ad895cc2f0cdc7
Vasile Falamas <vasile.falamas@hotmail.com>
2015-02-16 23:32:41 +0200

    Added Event entity,

---
+++ EventsNow.Connectivity/Evemt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventsNow.Connectivity
+{
+    public class Evemt
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
