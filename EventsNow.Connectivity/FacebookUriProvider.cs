183b3d4f9e9f9434b0b8318c413cd1502938772c
Vasile Falamas <vasile.falamas@hotmail.com>
2015-02-17 01:09:21 +0200

    Added Permissions for connection.

--- EventsNow.Connectivity/FacebookUriProvider.cs
+++ EventsNow.Connectivity/FacebookUriProvider.cs
@@ -5,6 +5,17 @@ namespace EventsNow.Connectivity
 {
     public class FacebookUriProvider
     {
+        private const string AppId = "34234234234";
+
+        private const string ProductId = "msft-342342342"; 
+
+        public static Uri GetConnectionUri()
+        {
+            var defaultPermissions = string.Join(",", new[] { Permissions.PublicProfile, Permissions.ReadStream, Permissions.UserEvents, Permissions.UserLocation });
+
+            return new Uri(string.Format(@"fbconnect://authorize?client_id={0}&scope={1}&redirect_uri=msft-{2}://authorize", AppId, defaultPermissions, ProductId));
+        }
+
         public static Uri GetUserNameInfoUri(AccessToken accessToken)
         {
             return new Uri(string.Format("https://graph.facebook.com/me?access_token={0}", accessToken.Value));
